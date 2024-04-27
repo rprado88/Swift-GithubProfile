f90084cf1ac4974b3e272e566d5cca73a574b27b
Rodrigo Prado <32552146+rprado88@users.noreply.github.com>
2024-04-27 16:48:10 -0700

    Initial Commit

---
+++ GithubProfile/GithubProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubProfileApp.swift
+//  GithubProfile
+//
+//  Created by Rodrigo Prado on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
